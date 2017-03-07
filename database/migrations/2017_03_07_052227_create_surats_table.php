cdf9d3f47f991691176e7b7c15628f795d9b74c9
Arukio <ragnaboyx@gmail.com>
2017-03-07 12:56:31 +0700

    Penambahan Migration database

---
+++ database/migrations/2017_03_07_052227_create_surats_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('no_agenda');
+            $table->string('kode');
+            $table->string('tujuan');
+            $table->string('no_surat');
+            $table->string('tgl_surat');
+            $table->text('isi_ringkas');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surats');
+    }
+}
